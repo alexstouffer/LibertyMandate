bee378d69a8ed6d17b2b09049c7cde11d72a1551
alexstouffer <bizkite@hotmail.com>
2021-09-25 11:21:43 -0700

    refactor menu style before scripting

---
+++ docs/js/toggleMenu.js
@@ -0,0 +1,10 @@
+const mobileMenuButton = document.getElementById('mobileMenuButton');
+const mobileMenuList = document.getElementById('hamitems');
+const ptest = document.getAnimations('ptest');
+
+function toggleMenu() {
+    mobileMenuList.classList.toggle('hidden');
+    console.log('toggleMenu fired');
+    ptest.innerText = 'injected text';
+}
+
