aba0933813af4b0a0608b829620d1d58e1a2dc4a
alexstouffer <bizkite@hotmail.com>
2021-10-21 01:22:33 -0700

    update front page to include community section

--- content/article/contributions.md
+++ content/article/contributions.md
@@ -9,3 +9,5 @@ authors:
 
 1. Coders: Help us build the infrastructure to get more people posters by contributing pull requests to [our repository](https://github.com/alexstouffer/LibertyMandate) on a new branch, and addressing [issues](https://github.com/alexstouffer/LibertyMandate/issues) that hinder our progress.
 2. Everyone: Invite people you know to join our [Telegram Channel](https://t.me/SoCalMandateProtest) where they can get updates from us as things progress.
+3. Contribute Information about legal funds, trade organizations, and Non-Profits collaborating in the fight against vaccine mandates by using our form (pending). Once more people contribute to the listing we can allow users to find pending lawsuits in their location and contribute to them directly.
+4. Add your email to our newsletter, and subscribe to our RSS Feed to receive updates from the Liberty Mandate Network.
--- docs/js/heroCarousel.js
+++ docs/js/heroCarousel.js
@@ -1,9 +1,39 @@
-const carouselContainer = document.getElementsByClassName("carousel-container")[0];
-const carouselImages = carouselContainer.getElementsByTagName('img');
+//get all the images and li from HTML
+var carouselImages = document.getElementsByClassName('carousel-image');
+var dotNode = document.getElementsByTagName("li");
+var currentIndex = 0;
+
 
 function displayImage() {
-    for (let j = 0; j < carouselImages.length; j++) {
-      carouselImages[j].style.display = "none";
-    }
-    carouselImages[currentIndex].style.display = "block";
+  for (let j = 0; j < carouselImages.length; j++) {
+    carouselImages[j].style.display = "none";
   }
+  carouselImages[currentIndex].style.display = "block";
+}
+
+function moveLeft() {
+  if (currentIndex == 0) {
+    currentIndex = carouselImages.length - 1;
+  } else {
+    currentIndex--;
+  }
+
+  displayImage();
+
+}
+
+function moveRight() {
+  if (currentIndex == carouselImages.length - 1) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+
+  displayImage();
+
+}
+
+window.setInterval(moveRight, 4000);
+
+//initial stage
+displayImage();
